dcdea64a063a66bfcc040ab9f5f58b86e8694e32
fuxy16 <fuxy16@mails.tsinghua.edu.cn>
2019-01-27 11:57:14 +0800

    imfill

--- draw.m
+++ draw.m
@@ -9,6 +9,8 @@ q=imopen(p,se);
 ppp=bwlabel(q,8);
 im=double(ppp==1);
 map=double(imresize(im,1/big_size)>0.8);
+map(20,41)=0;map(84,19)=0;map(62,71)=0;map(77,69)=0;map(69,89)=0;map(77,89)=0;map(38,159)=0;map(62,159)=0;map(69,21)=0;
+map(31,123)=1;
 imshow(map);
 door=[[37,91];[37,92];[37,93];[62,91];[62,92];[62,93];[20,41];[72,41]];
 for i=1:size(door,1)
